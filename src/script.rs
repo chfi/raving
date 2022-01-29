bd12938801e43ec77a2bfdbb9f81df0f962a1f27
Christian Fischer <christian@chfi.se>
2022-01-29 13:23:28 -0600

    add rhai constants for several vulkan types

--- src/script.rs
+++ src/script.rs
@@ -27,4 +27,97 @@ pub mod vk {
         layout!(COLOR_ATTACHMENT_OPTIMAL);
         layout!(PRESENT_SRC_KHR);
     }
+
+    #[rhai_mod(name = "DescriptorType")]
+    pub mod descriptor_type {
+        macro_rules! ty {
+            ($t:ident) => {
+                pub const $t: vk::DescriptorType = vk::DescriptorType::$t;
+            };
+        }
+
+        ty!(STORAGE_IMAGE);
+        ty!(COMBINED_IMAGE_SAMPLER);
+        ty!(SAMPLER);
+        ty!(STORAGE_BUFFER);
+        ty!(STORAGE_BUFFER_DYNAMIC);
+        ty!(STORAGE_TEXEL_BUFFER);
+        ty!(UNIFORM_BUFFER);
+        ty!(UNIFORM_TEXEL_BUFFER);
+        ty!(UNIFORM_BUFFER_DYNAMIC);
+        ty!(INPUT_ATTACHMENT);
+    }
+
+    #[rhai_mod(name = "ShaderStageFlags")]
+    pub mod shader_stage_flags {
+        macro_rules! flag {
+            ($f:ident) => {
+                pub const $f: vk::ShaderStageFlags = vk::ShaderStageFlags::$f;
+            };
+        }
+
+        flag!(COMPUTE);
+        flag!(VERTEX);
+        flag!(TESSELLATION_CONTROL);
+        flag!(TESSELLATION_EVALUATION);
+        flag!(GEOMETRY);
+        flag!(FRAGMENT);
+        flag!(ALL_GRAPHICS);
+        flag!(ALL);
+    }
+
+    #[rhai_mod(name = "PipelineStageFlags")]
+    pub mod pipeline_stage_flags {
+        macro_rules! flag {
+            ($f:ident) => {
+                pub const $f: vk::PipelineStageFlags =
+                    vk::PipelineStageFlags::$f;
+            };
+        }
+
+        flag!(TOP_OF_PIPE);
+        flag!(DRAW_INDIRECT);
+        flag!(VERTEX_INPUT);
+        flag!(VERTEX_SHADER);
+        flag!(TESSELLATION_CONTROL_SHADER);
+        flag!(TESSELLATION_EVALUATION_SHADER);
+        flag!(GEOMETRY_SHADER);
+        flag!(FRAGMENT_SHADER);
+        flag!(EARLY_FRAGMENT_TESTS);
+        flag!(LATE_FRAGMENT_TESTS);
+        flag!(COLOR_ATTACHMENT_OUTPUT);
+        flag!(COMPUTE_SHADER);
+        flag!(TRANSFER);
+        flag!(BOTTOM_OF_PIPE);
+        flag!(HOST);
+        flag!(ALL_GRAPHICS);
+        flag!(ALL_COMMANDS);
+    }
+
+    #[rhai_mod(name = "AccessFlags")]
+    pub mod access_flags {
+        macro_rules! flag {
+            ($f:ident) => {
+                pub const $f: vk::AccessFlags = vk::AccessFlags::$f;
+            };
+        }
+
+        flag!(INDIRECT_COMMAND_READ);
+        flag!(INDEX_READ);
+        flag!(VERTEX_ATTRIBUTE_READ);
+        flag!(UNIFORM_READ);
+        flag!(INPUT_ATTACHMENT_READ);
+        flag!(SHADER_READ);
+        flag!(SHADER_WRITE);
+        flag!(COLOR_ATTACHMENT_READ);
+        flag!(COLOR_ATTACHMENT_WRITE);
+        flag!(DEPTH_STENCIL_ATTACHMENT_READ);
+        flag!(DEPTH_STENCIL_ATTACHMENT_WRITE);
+        flag!(TRANSFER_READ);
+        flag!(TRANSFER_WRITE);
+        flag!(HOST_READ);
+        flag!(HOST_WRITE);
+        flag!(MEMORY_READ);
+        flag!(MEMORY_WRITE);
+    }
 }
